60146bd674c4112560800ee59f73c625c4a8d034
manikGit <srivastavamanik2@gmail.com>
2019-06-03 03:03:53 +0530

    1 file modified

--- components/CardsAccessible.js
+++ components/CardsAccessible.js
@@ -125,8 +125,8 @@ class CardsAccessible extends Component {
               ref={provided.innerRef}
               style={getListStyle(snapshot.isDraggingOver)}
             >
-              {this.state.items.map((item, index) => (
-                <Draggable key={`item-${index}`} draggableId={`item-${index}`} index={index}>
+              {this.state.items.map((data, i) => (
+                <Draggable key={`item-${i}`} draggableId={`item-${i}`} index={i}>
                   {(provided, snapshot) => (
                     <div
                       ref={provided.innerRef}
@@ -137,7 +137,40 @@ class CardsAccessible extends Component {
                         provided.draggableProps.style
                       )}
                     >
-                      {item.name}
+                      <List                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                      
+                       key={i} className="listStyle" style={{ paddingLeft: '2vh', paddingRight: '2vh' }}  >
+                <ListItem className="innerListStyle" style={{ outlineColor: '#293e40', padding: '0px' }} onDragOver={(e) => this.onDragOver(e, i)}
+                >
+                  <Card draggable
+                    onDragStart={e => this.onDragStart(e, i)}
+                    onDragEnd={e => this.onDragEnd(e)}
+                  >
+                    <CardHeader
+                      avatar={
+                        <Avatar alt={data.name} src={data.photo} />
+                      }
+                      title={data.name + " " + data.surname}
+                      subheader={data.email}
+                    >
+                    </CardHeader>
+                    <CardContent>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        Gender: {data.gender}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        Age: {data.age}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </ListItem>
+              </List>
                     </div>
                   )}
                 </Draggable>
